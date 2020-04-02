e5879410c25bc7547a0416c22d9fac070715325f
jack-gu <gfjack321@gmail.com>
2020-04-02 16:25:55 +0800

    simple kafka producer using spring boot

---
+++ src/main/java/com/jackgu/kafkaproducer/model/User.java
@@ -0,0 +1,38 @@
+package com.jackgu.kafkaproducer.model;
+
+public class User {
+
+	private String name;
+	private Long age;
+	private Long salary;
+
+	public User(String name, Long age, Long salary) {
+		this.name = name;
+		this.age = age;
+		this.salary = salary;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Long getAge() {
+		return age;
+	}
+
+	public void setAge(Long age) {
+		this.age = age;
+	}
+
+	public Long getSalary() {
+		return salary;
+	}
+
+	public void setSalary(Long salary) {
+		this.salary = salary;
+	}
+}
